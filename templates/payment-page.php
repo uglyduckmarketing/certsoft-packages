a94e3c3e80e9c7cb443e09007b97ec54e83c2ab3
Matt Lewis <matt@ugduck.com>
2024-01-23 11:47:26 -0800

    Updated Name Inputs

--- templates/payment-page.php
+++ templates/payment-page.php
@@ -7,13 +7,14 @@
     <h2 class="sr-only">Checkout</h2>
 
     <form class="lg:grid lg:grid-cols-2 lg:gap-x-12 xl:gap-x-16 faded">
-      <div>
 
+      <div clas="ticket-information">
+
+      <?php if( have_rows('payment_options', 'options') ) : while( have_rows('payment_options', 'options') ): the_row(); ?>
+      <?php if(!get_sub_field('hide_ticket_information')) : ?>
         <!-- County / Court Information -->
-        <div>
-
+        <div class="mb-10 border-b border-gray-200 pb-10">
           <h2 class="text-2xl md:text-3xl font-medium text-gray-900 block mb-6">Ticket Information</h2>
-
           <div class="mt-4 grid md:flex gap-3 items-end w-full">
             <div class="w-full">
               <label>Where Did You Get Your Ticket?</label>
@@ -31,11 +32,28 @@
               </select>
             </div>
           </div>
+
+          <div class="mt-6">
+            <label for="due_date">Certificate Due Date</label>
+            <div class="mt-1">
+              <input type="date" name="due_date" id="due_date">
+            </div>
+          </div>
+
+          <div class="mt-6">
+            <label for="case_number">Case Or Docket Number</label>
+            <div class="mt-1">
+              <input type="text" name="case_number" id="case_number">
+            </div>
+          </div>
+
         </div>
+        <?php endif; ?>
+        <?php endwhile; endif; ?>
 
 
 
-        <div class="mt-10 border-t border-gray-200 pt-10">
+        <div class="student-details">
 
           <h2 class="text-2xl md:text-3xl font-medium text-gray-900 block mb-6">Student Details</h2>
 
@@ -58,6 +76,13 @@
               <label for="email">Email Address</label>
               <div class="mt-1">
                 <input type="email" name="email" id="email" autocomplete="email">
+              </div>
+            </div>
+
+            <div class="sm:col-span-2">
+              <label for="email">Confirm Email Address</label>
+              <div class="mt-1">
+                <input type="email" name="confirm_email" id="confirm_email" autocomplete="email">
               </div>
             </div>
 
@@ -145,65 +170,13 @@
                 <option value="WY">Wyoming</option>
                 </select>
               </div>
+            </div>
             <?php endif; ?>
             <?php endwhile; endif; ?>
 
-            </div>
           </div>
-        </div>
 
-        <!-- Payment -->
-        <div class="mt-10 border-t border-gray-200 pt-10">
-
-          <h2 class="text-2xl md:text-3xl font-medium text-gray-900 block mb-6">Payment</h2>
-
-          <button type="button" class="flex w-full items-center justify-center rounded-md border border-transparent bg-black py-3 text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2">
-          <span class="sr-only">Pay with Apple Pay</span>
-          <svg class="h-4 w-auto" fill="currentColor" viewBox="0 0 50 20">
-            <path d="M9.536 2.579c-.571.675-1.485 1.208-2.4 1.132-.113-.914.334-1.884.858-2.484C8.565.533 9.564.038 10.374 0c.095.951-.276 1.884-.838 2.579zm.829 1.313c-1.324-.077-2.457.751-3.085.751-.638 0-1.6-.713-2.647-.694-1.362.019-2.628.79-3.323 2.017-1.429 2.455-.372 6.09 1.009 8.087.676.99 1.485 2.075 2.552 2.036 1.009-.038 1.409-.656 2.628-.656 1.228 0 1.58.656 2.647.637 1.104-.019 1.8-.99 2.475-1.979.771-1.122 1.086-2.217 1.105-2.274-.02-.019-2.133-.828-2.152-3.263-.02-2.036 1.666-3.007 1.742-3.064-.952-1.408-2.437-1.56-2.951-1.598zm7.645-2.76v14.834h2.305v-5.072h3.19c2.913 0 4.96-1.998 4.96-4.89 0-2.893-2.01-4.872-4.885-4.872h-5.57zm2.305 1.941h2.656c2 0 3.142 1.066 3.142 2.94 0 1.875-1.142 2.95-3.151 2.95h-2.647v-5.89zM32.673 16.08c1.448 0 2.79-.733 3.4-1.893h.047v1.779h2.133V8.582c0-2.14-1.714-3.52-4.351-3.52-2.447 0-4.256 1.399-4.323 3.32h2.076c.171-.913 1.018-1.512 2.18-1.512 1.41 0 2.2.656 2.2 1.865v.818l-2.876.171c-2.675.162-4.123 1.256-4.123 3.159 0 1.922 1.495 3.197 3.637 3.197zm.62-1.76c-1.229 0-2.01-.59-2.01-1.494 0-.933.752-1.475 2.19-1.56l2.562-.162v.837c0 1.39-1.181 2.379-2.743 2.379zM41.1 20c2.247 0 3.304-.856 4.227-3.454l4.047-11.341h-2.342l-2.714 8.763h-.047l-2.714-8.763h-2.409l3.904 10.799-.21.656c-.352 1.114-.923 1.542-1.942 1.542-.18 0-.533-.02-.676-.038v1.779c.133.038.705.057.876.057z" />
-          </svg>
-        </button>
-
-        <div class="relative mt-7">
-          <div class="absolute inset-0 flex items-center" aria-hidden="true">
-            <div class="w-full border-t border-gray-200"></div>
-          </div>
-          <div class="relative flex justify-center">
-            <span class="bg-gray-50 px-4 text-sm font-medium text-gray-500">or</span>
-          </div>
-        </div>
-
-          <div class="mt-6 grid grid-cols-4 gap-x-4 gap-y-3">
-            <div class="col-span-4">
-              <label for="card-number">Card Number</label>
-              <div class="mt-1">
-                <input type="text" id="card-number" name="card-number" autocomplete="cc-number">
-              </div>
-            </div>
-
-            <div class="col-span-4">
-              <label for="name-on-card">Name On Card</label>
-              <div class="mt-1">
-                <input type="text" id="name-on-card" name="name-on-card" autocomplete="cc-name">
-              </div>
-            </div>
-
-            <div class="col-span-3">
-              <label for="expiration-date">Expiration Date (MM/YY)</label>
-              <div class="mt-1">
-                <input type="text" name="expiration-date" id="expiration-date" autocomplete="cc-exp">
-              </div>
-            </div>
-
-            <div>
-              <label for="cvc">CVC</label>
-              <div class="mt-1">
-                <input type="text" name="cvc" id="cvc" autocomplete="csc">
-              </div>
-            </div>
-
-
-            <div class="col-span-4 mt-8">
+          <div class="col-span-4 mt-8">
               <div class="h-32 w-full p-5 leading-6 overflow-scroll text-sm border border-gray-200 rounded-lg shadow-sm text-gray-600">
                 <strong class="block mb-2">Go To www.ItsFastTrafficSchool.com Terms of Service</strong>
                 I certify under penalty of perjury that I alone will study the Go To www.ItsFastTrafficSchool.com course materials, take the quizzes and final exam without any outside assistance. I understand that if it is determined that I have not completed Go To www.ItsFastTrafficSchool.com without outside assistance, make false statements or present falsified documents to the court or Go To www.ItsFastTrafficSchool.com, this may be a felony and the completion certificate will not be accepted and I will receive no refund. I have reviewed the list of licensed traffic schools and understand that Go To www.ItsFastTrafficSchool.com is a DMV Licensed traffic school for the state of California in the English Language Only. I certify that I have a valid drivers license and am eligible for traffic school.
@@ -252,10 +225,73 @@
               </div>
             </div>
 
-            <div class="block mx-auto gap-2 h-6 items-center col-span-4 mt-4 justify-center">
+            <div class="flex mx-auto gap-2 h-6 items-center col-span-4 mt-4 justify-center">
               <div class="flex items-center gap-2">
                 <input id="terms" name="terms" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-600">
                 <span class="block text-sm font-medium text-gray-700">I Accept The Terms & Conditions</span>
+              </div>
+            </div>
+
+        </div>
+
+        <!-- Payment -->
+        <div class="mt-10 border-t border-gray-200 pt-10">
+
+          <h2 class="text-2xl md:text-3xl font-medium text-gray-900 block mb-6">Payment</h2>
+
+          <button type="button" class="flex w-full items-center justify-center rounded-md border border-transparent bg-black py-3 text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2">
+          <span class="sr-only">Pay with Apple Pay</span>
+          <svg class="h-4 w-auto" fill="currentColor" viewBox="0 0 50 20">
+            <path d="M9.536 2.579c-.571.675-1.485 1.208-2.4 1.132-.113-.914.334-1.884.858-2.484C8.565.533 9.564.038 10.374 0c.095.951-.276 1.884-.838 2.579zm.829 1.313c-1.324-.077-2.457.751-3.085.751-.638 0-1.6-.713-2.647-.694-1.362.019-2.628.79-3.323 2.017-1.429 2.455-.372 6.09 1.009 8.087.676.99 1.485 2.075 2.552 2.036 1.009-.038 1.409-.656 2.628-.656 1.228 0 1.58.656 2.647.637 1.104-.019 1.8-.99 2.475-1.979.771-1.122 1.086-2.217 1.105-2.274-.02-.019-2.133-.828-2.152-3.263-.02-2.036 1.666-3.007 1.742-3.064-.952-1.408-2.437-1.56-2.951-1.598zm7.645-2.76v14.834h2.305v-5.072h3.19c2.913 0 4.96-1.998 4.96-4.89 0-2.893-2.01-4.872-4.885-4.872h-5.57zm2.305 1.941h2.656c2 0 3.142 1.066 3.142 2.94 0 1.875-1.142 2.95-3.151 2.95h-2.647v-5.89zM32.673 16.08c1.448 0 2.79-.733 3.4-1.893h.047v1.779h2.133V8.582c0-2.14-1.714-3.52-4.351-3.52-2.447 0-4.256 1.399-4.323 3.32h2.076c.171-.913 1.018-1.512 2.18-1.512 1.41 0 2.2.656 2.2 1.865v.818l-2.876.171c-2.675.162-4.123 1.256-4.123 3.159 0 1.922 1.495 3.197 3.637 3.197zm.62-1.76c-1.229 0-2.01-.59-2.01-1.494 0-.933.752-1.475 2.19-1.56l2.562-.162v.837c0 1.39-1.181 2.379-2.743 2.379zM41.1 20c2.247 0 3.304-.856 4.227-3.454l4.047-11.341h-2.342l-2.714 8.763h-.047l-2.714-8.763h-2.409l3.904 10.799-.21.656c-.352 1.114-.923 1.542-1.942 1.542-.18 0-.533-.02-.676-.038v1.779c.133.038.705.057.876.057z" />
+          </svg>
+        </button>
+
+        <div class="relative mt-7">
+          <div class="absolute inset-0 flex items-center" aria-hidden="true">
+            <div class="w-full border-t border-gray-200"></div>
+          </div>
+          <div class="relative flex justify-center">
+            <span class="bg-gray-50 px-4 text-sm font-medium text-gray-500">or</span>
+          </div>
+        </div>
+
+
+          <div class="mt-6 grid grid-cols-4 gap-x-4 gap-y-3">
+
+          <div class="flex hidden col-span-4 items-center gap-3 mb-4" id="name-container">
+            <h2 class="text-2xl" id="cc-name"></h2>
+            <button class="text-gray-500" id="edit-name">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-500">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+              </svg>
+            </button>
+          </div>
+
+          <div class="col-span-4" id="card-name">
+            <label for="name-on-card">Name On Card</label>
+            <div class="mt-1">
+              <input type="text" id="name-on-card" name="name-on-card" autocomplete="cc-name">
+            </div>
+          </div>
+
+            <div class="col-span-4">
+              <label for="card-number">Card Number</label>
+              <div class="mt-1">
+                <input type="text" id="card-number" name="card-number" autocomplete="cc-number">
+              </div>
+            </div>
+
+            <div class="col-span-3">
+              <label for="expiration-date">Expiration Date (MM/YY)</label>
+              <div class="mt-1">
+                <input type="text" name="expiration-date" id="expiration-date" autocomplete="cc-exp">
+              </div>
+            </div>
+
+            <div>
+              <label for="cvc">CVC</label>
+              <div class="mt-1">
+                <input type="text" name="cvc" id="cvc" autocomplete="csc">
               </div>
             </div>
 
@@ -306,6 +342,38 @@
     </form>
   </div>
 </div>
+
+<script>
+  const lastName = document.getElementById('last-name');
+  const firstName = document.getElementById('first-name');
+  const ccName = document.getElementById('cc-name');
+  const ccInput = document.getElementById('card-name');
+  const nameContainer = document.getElementById('name-container');
+  const editButton = document.getElementById('edit-name');
+  const nameOnCard = document.getElementById('name-on-card');
+
+  lastName.addEventListener('blur', function() {
+
+    if(lastName.value && firstName.value) {
+      nameContainer.classList.remove('hidden');
+      ccName.innerHTML = firstName.value + ' ' + lastName.value;
+      nameOnCard.value = firstName.value + ' ' + lastName.value;
+      ccInput.classList.add('hidden');
+    }
+    
+  });
+
+  editButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    ccInput.classList.remove('hidden');
+  })
+
+  nameOnCard.addEventListener('change', function() {
+    ccName.innerHTML = nameOnCard.value;
+  })
+
+
+</script>
 
 <style>
   #header, #footer {
