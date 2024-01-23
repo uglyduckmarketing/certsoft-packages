8a506bc32cba400c967c1e676c4c405799b822dc
Matt Lewis <matt@ugduck.com>
2024-01-23 13:58:22 -0800

    Updated packages steps

--- templates/headers/header-01.php
+++ templates/headers/header-01.php
@@ -7,7 +7,31 @@
       </div>
       <?php endwhile; endif; ?>
       <div class="font-semibold text-sm text-gray-700 hidden md:block"><?php echo get_field('school_name'); ?></div>
-      <div class="font-semibold text-xs md:text-sm text-gray-700">Returning Customer? <a href="<?php echo get_field('school_login_link'); ?>" class="text-blue-600">Login</a></div>
+      <div class="font-semibold text-xs md:text-sm text-gray-700">Returning Customer? <a href="<?php echo get_field('school_login_link'); ?>" class="text-blue-600 text-primary">Login</a></div>
     </div>
   </div>
 </header>
+
+<style>
+  <?php if( have_rows('school_information', 'options') ) : while( have_rows('school_information', 'options') ): the_row(); ?>
+  <?php if(get_sub_field('school_primary_color')) : ?>
+    .text-primary {
+      color: <?php the_sub_field('school_primary_color'); ?>;
+    }
+    .bg-primary {
+      background: <?php the_sub_field('school_primary_color'); ?>;
+      transition: all .3s ease;
+    }
+    .bg-primary:hover {
+      background: <?php the_sub_field('school_primary_color'); ?>;
+      filter: brightness(110%);
+    }
+    input:focus, textarea:focus { 
+      outline: 1px solid <?php the_sub_field('school_primary_color'); ?>; 
+    }
+    .is-editable svg {
+      color: <?php the_sub_field('school_primary_color'); ?>;
+    }
+  <?php endif; ?>
+  <?php endwhile; endif; ?>
+</style>
--- templates/payment-page.php
+++ templates/payment-page.php
@@ -4,16 +4,15 @@
 
 <div class="bg-white">
   <div class="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8 py-8 md:py-16">
-    <h2 class="sr-only">Checkout</h2>
 
     <form class="lg:grid lg:grid-cols-2 lg:gap-x-12 xl:gap-x-16 faded">
 
-      <div clas="ticket-information">
+      <div class="form-container mt-4">
 
       <?php if( have_rows('payment_options', 'options') ) : while( have_rows('payment_options', 'options') ): the_row(); ?>
       <?php if(!get_sub_field('hide_ticket_information')) : ?>
         <!-- County / Court Information -->
-        <div class="mb-10 border-b border-gray-200 pb-10">
+        <div class="mb-10 border-b border-gray-200 pb-10 ticket-info hidden">
           <h2 class="text-2xl md:text-3xl font-medium text-gray-900 block mb-6">Ticket Information</h2>
           <div class="mt-4 grid md:flex gap-3 items-end w-full">
             <div class="w-full">
@@ -53,9 +52,21 @@
 
 
 
-        <div class="student-details">
+        <div class="student-details" id="step-01">
 
-          <h2 class="text-2xl md:text-3xl font-medium text-gray-900 block mb-6">Student Details</h2>
+          <div class="step-heading flex justify-between items-center">
+            <div>
+              <span class="text-primary text-xs mb-2 block font-semibold">Step 01.</span>
+              <h2 class="text-2xl md:text-3xl font-medium text-gray-900 block mb-6">Student Details</h2>
+            </div>
+            <button id="step-01-edit">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-500">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+              </svg>
+            </button>
+          </div>
+
+          <div class="step-01-container">
 
           <div class="mt-4 grid grid-cols-1 gap-y-3 sm:grid-cols-2 sm:gap-x-4">
             <div>
@@ -177,7 +188,7 @@
           </div>
 
           <div class="col-span-4 mt-8">
-              <div class="h-32 w-full p-5 leading-6 overflow-scroll text-sm border border-gray-200 rounded-lg shadow-sm text-gray-600">
+              <div class="h-32 w-full p-5 leading-6 overflow-scroll text-sm border border-gray-400 rounded-lg shadow-sm text-gray-600">
                 <strong class="block mb-2">Go To www.ItsFastTrafficSchool.com Terms of Service</strong>
                 I certify under penalty of perjury that I alone will study the Go To www.ItsFastTrafficSchool.com course materials, take the quizzes and final exam without any outside assistance. I understand that if it is determined that I have not completed Go To www.ItsFastTrafficSchool.com without outside assistance, make false statements or present falsified documents to the court or Go To www.ItsFastTrafficSchool.com, this may be a felony and the completion certificate will not be accepted and I will receive no refund. I have reviewed the list of licensed traffic schools and understand that Go To www.ItsFastTrafficSchool.com is a DMV Licensed traffic school for the state of California in the English Language Only. I certify that I have a valid drivers license and am eligible for traffic school.
                 <br /><br />
@@ -232,26 +243,51 @@
               </div>
             </div>
 
+            <div class="mt-6 col-span-4">
+              <button id="student-details-confirm" class="w-full rounded-md border border-transparent bg-blue-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-50 flex gap-4 items-center justify-center bg-primary">
+                Next step
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                </svg>
+              </button>
+            </div>
+
+          </div>
+
         </div>
 
         <!-- Payment -->
-        <div class="mt-10 border-t border-gray-200 pt-10">
+        <div class="mt-10 is-editable" id="step-02">
 
-          <h2 class="text-2xl md:text-3xl font-medium text-gray-900 block mb-6">Payment</h2>
+          <div class="step-heading flex justify-between items-center">
+            <div>
+              <span class="text-primary text-xs mb-2 block font-semibold">Step 02.</span>
+              <h2 class="text-2xl md:text-3xl font-medium text-gray-900 block mb-6">Payment</h2>
+            </div>
+            <button id="step-02-edit">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-500">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+              </svg>
+            </button>
+          </div>
 
-          <button type="button" class="flex w-full items-center justify-center rounded-md border border-transparent bg-black py-3 text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2">
-          <span class="sr-only">Pay with Apple Pay</span>
-          <svg class="h-4 w-auto" fill="currentColor" viewBox="0 0 50 20">
-            <path d="M9.536 2.579c-.571.675-1.485 1.208-2.4 1.132-.113-.914.334-1.884.858-2.484C8.565.533 9.564.038 10.374 0c.095.951-.276 1.884-.838 2.579zm.829 1.313c-1.324-.077-2.457.751-3.085.751-.638 0-1.6-.713-2.647-.694-1.362.019-2.628.79-3.323 2.017-1.429 2.455-.372 6.09 1.009 8.087.676.99 1.485 2.075 2.552 2.036 1.009-.038 1.409-.656 2.628-.656 1.228 0 1.58.656 2.647.637 1.104-.019 1.8-.99 2.475-1.979.771-1.122 1.086-2.217 1.105-2.274-.02-.019-2.133-.828-2.152-3.263-.02-2.036 1.666-3.007 1.742-3.064-.952-1.408-2.437-1.56-2.951-1.598zm7.645-2.76v14.834h2.305v-5.072h3.19c2.913 0 4.96-1.998 4.96-4.89 0-2.893-2.01-4.872-4.885-4.872h-5.57zm2.305 1.941h2.656c2 0 3.142 1.066 3.142 2.94 0 1.875-1.142 2.95-3.151 2.95h-2.647v-5.89zM32.673 16.08c1.448 0 2.79-.733 3.4-1.893h.047v1.779h2.133V8.582c0-2.14-1.714-3.52-4.351-3.52-2.447 0-4.256 1.399-4.323 3.32h2.076c.171-.913 1.018-1.512 2.18-1.512 1.41 0 2.2.656 2.2 1.865v.818l-2.876.171c-2.675.162-4.123 1.256-4.123 3.159 0 1.922 1.495 3.197 3.637 3.197zm.62-1.76c-1.229 0-2.01-.59-2.01-1.494 0-.933.752-1.475 2.19-1.56l2.562-.162v.837c0 1.39-1.181 2.379-2.743 2.379zM41.1 20c2.247 0 3.304-.856 4.227-3.454l4.047-11.341h-2.342l-2.714 8.763h-.047l-2.714-8.763h-2.409l3.904 10.799-.21.656c-.352 1.114-.923 1.542-1.942 1.542-.18 0-.533-.02-.676-.038v1.779c.133.038.705.057.876.057z" />
-          </svg>
-        </button>
+
+          <div class="step-02-container">
+          <div>
+            <button type="button" class="flex w-full items-center justify-center rounded-md border border-transparent bg-black py-3 text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2 apple-button">
+            <span class="sr-only">Pay with Apple Pay</span>
+            <svg class="h-4 w-auto" fill="currentColor" viewBox="0 0 50 20">
+              <path d="M9.536 2.579c-.571.675-1.485 1.208-2.4 1.132-.113-.914.334-1.884.858-2.484C8.565.533 9.564.038 10.374 0c.095.951-.276 1.884-.838 2.579zm.829 1.313c-1.324-.077-2.457.751-3.085.751-.638 0-1.6-.713-2.647-.694-1.362.019-2.628.79-3.323 2.017-1.429 2.455-.372 6.09 1.009 8.087.676.99 1.485 2.075 2.552 2.036 1.009-.038 1.409-.656 2.628-.656 1.228 0 1.58.656 2.647.637 1.104-.019 1.8-.99 2.475-1.979.771-1.122 1.086-2.217 1.105-2.274-.02-.019-2.133-.828-2.152-3.263-.02-2.036 1.666-3.007 1.742-3.064-.952-1.408-2.437-1.56-2.951-1.598zm7.645-2.76v14.834h2.305v-5.072h3.19c2.913 0 4.96-1.998 4.96-4.89 0-2.893-2.01-4.872-4.885-4.872h-5.57zm2.305 1.941h2.656c2 0 3.142 1.066 3.142 2.94 0 1.875-1.142 2.95-3.151 2.95h-2.647v-5.89zM32.673 16.08c1.448 0 2.79-.733 3.4-1.893h.047v1.779h2.133V8.582c0-2.14-1.714-3.52-4.351-3.52-2.447 0-4.256 1.399-4.323 3.32h2.076c.171-.913 1.018-1.512 2.18-1.512 1.41 0 2.2.656 2.2 1.865v.818l-2.876.171c-2.675.162-4.123 1.256-4.123 3.159 0 1.922 1.495 3.197 3.637 3.197zm.62-1.76c-1.229 0-2.01-.59-2.01-1.494 0-.933.752-1.475 2.19-1.56l2.562-.162v.837c0 1.39-1.181 2.379-2.743 2.379zM41.1 20c2.247 0 3.304-.856 4.227-3.454l4.047-11.341h-2.342l-2.714 8.763h-.047l-2.714-8.763h-2.409l3.904 10.799-.21.656c-.352 1.114-.923 1.542-1.942 1.542-.18 0-.533-.02-.676-.038v1.779c.133.038.705.057.876.057z" />
+            </svg>
+          </button>
+         </div>
 
         <div class="relative mt-7">
-          <div class="absolute inset-0 flex items-center" aria-hidden="true">
+          <div class="absolute inset-0 flex items-center or-title" aria-hidden="true">
             <div class="w-full border-t border-gray-200"></div>
           </div>
           <div class="relative flex justify-center">
-            <span class="bg-gray-50 px-4 text-sm font-medium text-gray-500">or</span>
+            <span class="bg-white px-4 text-sm font-medium text-gray-500">or</span>
           </div>
         </div>
 
@@ -294,6 +330,8 @@
                 <input type="text" name="cvc" id="cvc" autocomplete="csc">
               </div>
             </div>
+            
+            </div>
 
           </div>
         </div>
@@ -302,21 +340,21 @@
       <!-- Order summary -->
       <div class="mt-10 lg:mt-0">
 
-        <div class="mt-4 rounded-lg border border-gray-200 bg-white shadow-sm sticky top-6 faded">
+        <div class="mt-4 rounded-lg border border-gray-300 bg-white shadow-sm sticky top-6 faded">
           <h3 class="sr-only">Items in your cart</h3>
           <div class="p-6">
-            <span class="mb-4 mt-2 text-gray-400 text-xs block">Traffic School Package</span>
+            <span class="mb-4 mt-2 text-gray-400 text-xs block">Selected Package</span>
             <h2 class="text-3xl mb-4 md:pr-12">California Traffic School: Pass Guarantee, DMV Confirmation</h2>
             <p class="text-base text-gray-600">Licensed by the DMV, we have helped over 1 million drivers mask their traffic ticket. We electronic filing of your certificate at the DMV & court. The course is an open-book final exam with only 6 short chapters and 25 easy questions!</p>
             <ul class="text-sm mt-6 text-gray-600 grid gap-3">
               <li class="flex gap-1.5 pr-6">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-blue-600">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-blue-600 text-primary">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
                 Plus, Guaranteed Pass Policy = Take it until you pass for free. NO additional fees required. We will notify the DMV & Court of Completion INSTANTLY.
               </li>
               <li class="flex gap-1.5 pr-6">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-blue-600">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-blue-600 text-primary">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
                 Plus, DMV Processed Verification = Our heroic support staff will monitor your ticket and confirm that it was successfully processed with the DMV and Court and will email you a confirmation number.</li>
@@ -330,7 +368,7 @@
           </dl>
 
           <div class="border-t border-gray-200 px-4 py-6 sm:px-6">
-            <button type="submit" class="w-full rounded-md border border-transparent bg-blue-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-50 flex gap-4 items-center justify-center">
+            <button type="submit" class="w-full rounded-md border border-transparent bg-blue-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-50 flex gap-4 items-center justify-center bg-primary">
               Purchase Course
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
@@ -351,6 +389,7 @@
   const nameContainer = document.getElementById('name-container');
   const editButton = document.getElementById('edit-name');
   const nameOnCard = document.getElementById('name-on-card');
+  const studentDetailsConfirmation = document.getElementById('student-details-confirm');
 
   lastName.addEventListener('blur', function() {
 
@@ -366,13 +405,19 @@
   editButton.addEventListener('click', function(e) {
     e.preventDefault();
     ccInput.classList.remove('hidden');
-  })
+  });
 
   nameOnCard.addEventListener('change', function() {
     ccName.innerHTML = nameOnCard.value;
-  })
+  });
 
-
+  studentDetailsConfirmation.addEventListener('click', function(e) {
+    e.preventDefault();
+    // document.getElementById('step-01').classList.add('hidden');
+    document.getElementById('step-01').classList.toggle('is-editable');
+    document.getElementById('step-02').classList.toggle('is-editable');
+  });
+  
 </script>
 
 <style>
@@ -413,6 +458,58 @@
     background-size: 1em;
   }
 
+
+  .is-editable {
+    transition: all .3s ease;
+    cursor: pointer;
+    border: 1px solid rgba(0,0,0,.25) !important;
+    box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
+    border-radius: 0.375rem;
+    width: 100%;
+    padding: 1.5rem 0;
+  }
+  .step-heading, .step-01-container, .step-02-container {
+    padding: 0 1rem !important;
+  }
+  .is-editable .step-heading {
+    transition: all .3s ease;
+  } 
+  .step-heading {
+    padding: 0;
+    transition: all .3s ease;
+  }
+  #step-02 .step-02-container {
+    display: block;
+  }
+  .is-editable .step-02-container, .is-editable .step-01-container {
+    display: none !important;
+  }
+  .is-editable h2 {
+    margin-bottom: 0;
+  }
+  .is-editable:hover {
+    background: #f9fafb;
+    box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+  }
+  .or-title {
+    display: flex !important;
+  }
+
+  #step-01, #step-02 {
+    transition: all .3s ease;
+  }
+
+  .apple-button svg {
+    color: white !important;
+  }
+
+  #step-01-edit svg, #step-02-edit svg {
+    display: none;
+  }
+  .is-editable #step-01-edit svg, .is-editable #step-02-edit svg {
+    display: block;
+  }
+  
 </style>
 
 <?php get_footer(); ?>
