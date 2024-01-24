32bd512a48a9efaa3e6e02b2aec7804e476f97e5
Matt Lewis <matt@ugduck.com>
2024-01-24 00:52:18 -0800

    Update payment-page.php

--- templates/payment-page.php
+++ templates/payment-page.php
@@ -474,7 +474,7 @@
     document.getElementById('step-01').classList.toggle('is-editable');
     document.getElementById('step-02').classList.toggle('is-editable');
     document.getElementById('progress-button-02').classList.toggle('active-progress');
-    progressBar.style.width = '10%';
+    progressBar.style.width = '10.25%';
   });
 
   progressTwoButton.addEventListener('click', function(e) {
